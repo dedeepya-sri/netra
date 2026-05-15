16d2e059326e64f55c8c7a6272418be2485c64ab
Sri Dedeepya Singampalli <sridedeepya524@gmail.com>
2026-05-15 21:08:11 +0530

    next js fix

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,8 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   reactCompiler: true,
+
+  output: "standalone",
+
+  experimental: {
+    serverActions: {
+      bodySizeLimit: "10mb",
+    },
+  },
 };
 
 export default nextConfig;
